5596a5005303ccdec087a09a856e0d77c82fe652
Seiji Tonouchi <s.tonouchi@gmail.com>
2025-10-05 13:09:27 +1300

    iroiro

--- scripts/00007_install_snap_pkgs.sh
+++ scripts/00007_install_snap_pkgs.sh
@@ -3,9 +3,31 @@
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 source ${SCRIPT_DIR}/../functions.sh
 
+## --classicがいるパッケージ
 for i in $(echo 'shotcut')
 do
-  INSTALLATION=$(dpkg -l | grep "^ii  ${i} .*$" | wc -l)
+  INSTALLATION=$(snap list ${i} | grep latest/stable | wc -l)
+  if [ ${INSTALLATION} -eq 0 ]
+  then
+    echo -n "installing $i ... "
+    sudo snap install $i --classic > /dev/null 2>&1
+    result=$?
+    if [ $result -eq 0 ]
+    then
+      show_success_message
+    else
+      show_error_message
+      exit 9
+    fi
+  else
+    echo "$i is already installed"
+  fi
+done
+
+## そのまま入れるパッケージ
+for i in $(echo 'intel-npu-driver')
+do
+  INSTALLATION=$(snap list ${i} | grep latest/stable | wc -l)
   if [ ${INSTALLATION} -eq 0 ]
   then
     echo -n "installing $i ... "
--- scripts/90001_update_desktop.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# 必ずやる処理
-SCRIPT_DIR=$(cd $(dirname $0);pwd)
-source ${SCRIPT_DIR}/../functions.sh
-
-update-desktop-database ~/.local/share/applications
-
-exit 1
